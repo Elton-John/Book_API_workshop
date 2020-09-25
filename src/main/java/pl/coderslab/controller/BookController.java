924b40159019d55da8153b6db1a7eea3425d06b9
Olga <nauka.dinosaurus@gmail.com>
2020-09-25 07:27:58 +0200

    add getBooks method

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,26 @@
+package pl.coderslab.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.entity.Book;
+import pl.coderslab.interfacies.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getBooks(){
+        return bookService.getBooks();
+    }
+}
--- src/main/java/pl/coderslab/service/MemoryBookService.java
+++ src/main/java/pl/coderslab/service/MemoryBookService.java
@@ -1,14 +1,30 @@
 package pl.coderslab.service;
 
+import org.springframework.stereotype.Component;
 import pl.coderslab.entity.Book;
 import pl.coderslab.interfacies.BookService;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MemoryBookService implements BookService {
+    private List<Book> books;
+    private static Long nextId;
+
+    public MemoryBookService() {
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        books.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+        nextId = 4L;
+    }
+
     @Override
     public List<Book> getBooks() {
-        return null;
+        return this.books;
     }
 
     @Override
