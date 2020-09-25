96fe3a15d1cd7ed1722adc65f49839562d8ba222
Olga <nauka.dinosaurus@gmail.com>
2020-09-25 06:57:04 +0200

    add config

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-@ComponentScan("pl/coderslab")
+@ComponentScan("pl.coderslab")
 @EnableWebMvc
 public class AppConfig  implements WebMvcConfigurer {
 }
